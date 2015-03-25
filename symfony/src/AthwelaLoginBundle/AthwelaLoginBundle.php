bf582f05d3b65ea7517d8c1f13db8a27033cfb90
Nipuna-Sankalpa <nipuna92@yahoo.com>
2015-03-26 02:50:12 +0530

    loginbundlecreated

---
+++ symfony/src/AthwelaLoginBundle/AthwelaLoginBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AthwelaLoginBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AthwelaLoginBundle extends Bundle
+{
+}
---
+++ symfony/src/AthwelaLoginBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AthwelaLoginBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('AthwelaLoginBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
