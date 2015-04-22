22834024a31224c8a0cd20ce7a54e17cbfc6f1df
Pubudu Fernando <pubudu.fernando@gmail.com>
2015-04-22 21:31:15 +0530

    minor edit

--- symfony/src/Athwela/TimelineBundle/Queries/TimelineQueryBuilder.php
+++ symfony/src/Athwela/TimelineBundle/Queries/TimelineQueryBuilder.php
@@ -13,7 +13,7 @@ use Athwela\DA\CustomQuery\CustomQuery;
 class TimelineQueryBuilder {
 
     public function getAvailableProjects() {
-        $query = "SELECT name, title, project.description, project.status, start_date, end_date, volunteers_needed, no_of_filled_positions, posted_date FROM `organization` INNER JOIN project ON organization.ID = project.o_ID ORDER BY posted_date DESC";
+        $query = "SELECT project.ID,organization.name, title, project.description, project.status, start_date, end_date, volunteers_needed, no_of_filled_positions, posted_date FROM `organization` INNER JOIN project ON organization.ID = project.o_ID ORDER BY posted_date DESC";
         $result = CustomQuery::getInstance() -> customQuery($query);
         return $result;
     }
