cd55d31e63f4f71fa4962e55b30825514124a476
DilipTharuka <diliptharuka@gmail.com>
2015-04-22 11:34:29 +0530

    errorfix

--- symfony/src/Athwela/ProfileSettings/UserBundle/Controller/VolunteerController.php
+++ symfony/src/Athwela/ProfileSettings/UserBundle/Controller/VolunteerController.php
@@ -70,6 +70,7 @@ class VolunteerController extends Controller {
 
     public function updatesAction(Request $request, $id) {
 
+        print_r($request);
         $data = $this->getRequest()->request->all();
         if(array_key_exists('file',$data))
         {
