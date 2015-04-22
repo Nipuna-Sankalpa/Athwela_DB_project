dbe9cb341c8cd033be901d185d862cddb2bb1be8
dilini-mampitiya <dilu92arachchi@gmail.com>
2015-04-22 21:19:18 +0530

    finalized volunteer profile

--- symfony/src/Athwela/DA/DBConnection.php
+++ symfony/src/Athwela/DA/DBConnection.php
@@ -39,7 +39,7 @@ class DBConnection {
 //open a connection with athwela dataBase
 
     public function getConnection() {
-        $connection = mysqli_connect('localhost', 'root', '0713899213', 'athwela');
+        $connection = mysqli_connect('localhost', 'root', '', 'athwela');
 
         if ($connection) {
             return $connection;
